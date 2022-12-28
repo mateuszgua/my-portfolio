3fb123652b4bfb4b07e0e2e40e0e7047a3401555
Mateusz Gula <mateuszgua@wp.eu>
2022-12-28 18:00:12 +0100

    Feat: add docker compose

--- app/config.py
+++ app/config.py
@@ -12,11 +12,11 @@ class Config:
     STATIC_FOLDER = 'static'
 
     HOST = '0.0.0.0'
-    PORT = 5001
+    PORT = 5050
     DEBUG = False
     TESTING = False
 
-    MAIL_SERVER='smtp.gmail.com'
+    MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
