f315acbc395b6fc4e439878fbff331cdccc49b7f
Mateusz Gula <mateuszgua@wp.eu>
2022-12-28 13:20:00 +0100

    Feat: add home view

--- app/views.py
+++ app/views.py
@@ -1,6 +1,15 @@
 from app import app
 
+from flask import render_template
+
+common = {
+    'first_name': 'Mateusz',
+    'last_name': 'Guła',
+    'alias': 'gua'
+}
 
 @app.route('/')
 def index():
-    return "Hello Mati"
+    return render_template('home.html', common=common)
+
+
