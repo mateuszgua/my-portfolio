7ca82798ca8f201357cd5ed3c7b9aca1f25419ea
Mateusz Gula <mateuszgua@wp.eu>
2022-12-28 14:10:20 +0100

    Feat:add projects view, add example projects in json, add base and navbar with bootstrap

--- app/views.py
+++ app/views.py
@@ -1,6 +1,10 @@
+import os
+import io
+import json
+
 from app import app
 
-from flask import render_template
+from flask import render_template, request
 
 common = {
     'first_name': 'Mateusz',
@@ -8,8 +12,36 @@ common = {
     'alias': 'gua'
 }
 
+
 @app.route('/')
 def index():
     return render_template('home.html', common=common)
 
 
+@app.route('/projects')
+def projects():
+    data = get_static_json("static/projects/projects.json")['projects']
+    data.sort(key=order_projects_by_weight, reverse=True)
+
+    tag = request.args.get('tags')
+    if tag is not None:
+        data = [project for project in data if tag.lower() in [project_tag.lower()
+                                                               for project_tag in project['tags']]]
+
+    return render_template('projects.html', common=common, projects=data, tag=tag)
+
+
+def order_projects_by_weight(projects):
+    try:
+        return int(projects['weight'])
+    except KeyError:
+        return 0
+
+
+def get_static_file(path):
+    site_root = os.path.realpath(os.path.dirname(__file__))
+    return os.path.join(site_root, path)
+
+
+def get_static_json(path):
+    return json.load(open(get_static_file(path)))
