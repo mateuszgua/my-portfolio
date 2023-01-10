ff400bcbdae9996d37edb4cb3d1bab981fc74f13
Mateusz Gula <mateuszgua@wp.eu>
2023-01-10 10:50:46 +0100

    Feat: add frontend in projects page, load data from json

--- app/views.py
+++ app/views.py
@@ -11,44 +11,21 @@ common = {
 }
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    message = {'greeting': 'Hello from Flask!'}
-    return jsonify(message)  # serialize and use JSON headers
-    # return render_template('home.html', common=common)
-
-
-@app.route('/user', methods=['GET', 'POST'])
-def user():
     with open("app/static/files/user.json", "r") as f:
         data = json.load(f)
     if request.method == 'GET':
         return jsonify(data)
 
 
-@app.route('/users', methods=["GET"])
-def users():
-    print("users endpoint reached...")
-    with open("users.json", "r") as f:
-        data = json.load(f)
-        data.append({
-            "username": "user4",
-            "pets": ["hamster"]
-        })
-        return jsonify(data)
-
-
-@app.route('/projects')
+@app.route('/projects', methods=["GET"])
 def projects():
-    data = Helper.get_static_json("static/projects/projects.json")['projects']
+    with open("app/static/files/projects.json", "r") as f:
+        data = json.load(f)
     data.sort(key=Helper.order_projects_by_weight, reverse=True)
-
-    tag = request.args.get('tags')
-    if tag is not None:
-        data = [project for project in data if tag.lower() in [project_tag.lower()
-                                                               for project_tag in project['tags']]]
-
-    return render_template('projects.html', common=common, projects=data, tag=tag)
+    if request.method == 'GET':
+        return jsonify(data)
 
 
 @app.route('/timeline')
